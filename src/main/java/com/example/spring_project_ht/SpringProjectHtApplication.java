963492cb26733076092c5c0d309e43758065b451
vladimirtodoshchak <vladimirtodoshchak@gmail.com>
2024-03-17 22:47:57 +0200

    Initial commit

---
+++ src/main/java/com/example/spring_project_ht/SpringProjectHtApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_project_ht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectHtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectHtApplication.class, args);
+    }
+
+}
